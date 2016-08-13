4a6dd358cd8db3c251d73380601d6a503627fa26
Eason Tsui <hibfnv@hotmail.com>
2016-08-13 12:52:58 +0800

    Rename string_format.py to string_format1.py

---
+++ string_format1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Learning about string format session
+width = int(raw_input('Enter a width number[10-50]: '))
+if width < 10 or width > 50:
+  print '\033[31mThe number is so bigger please enter number between 10-50.\033[0m
+else:
+ori_width = 12
+main_width = width - ori_width
+# define string format as title_format and main_format
+title_format = '%-*s%*s'
+main_format = '%-*s%*.2f'
+# split line in body
+print '\033[34m=\033[0m' * width
+print title_format % (main_width, 'Menu', ori_width, 'Price')
+# split line in body
+print '\033[34m-\033[0m' * width
+print main_format % (main_width, 'Apple', ori_width, 0.6)
+print main_format % (main_width, 'Banana', ori_width, 2)
+print main_format % (main_width, 'Carrot', ori_width, 6)
+print main_format % (main_width, 'Tomato', ori_width, 3.8)
+print main_format % (main_width, 'Pear', ori_width, 1.26)
+print main_format % (main_width, 'Peach', ori_width, 0.8)
+# end line
+print '\033[34m=\033[0m' * width
+print '\033[31mNotice: Please checkout your menu when leave out.\033[0m'
