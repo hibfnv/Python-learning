cd45832b92fe2d913d25a66b13a9907f3938672a
Eason Tsui <hibfnv@hotmail.com>
2016-08-13 13:24:43 +0800

    Create if_then.py

---
+++ if_then.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# This is a if justice script by python
+num = int(input('Enter a number[0-100]: '))
+x = 75
+if num == x :
+  print 'Greate!You have got a right number'
+else:
+  print 'Sorry, please try again next round...'
+print 'Done!'
