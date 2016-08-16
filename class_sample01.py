2bbebb456e08a7e0019088f116582884757fd419
Eason Tsui <hibfnv@hotmail.com>
2016-08-16 12:24:13 +0800

    Create class_sample01.py

---
+++ class_sample01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# define a class
+class Person:
+  def superman(self,name):
+    self.name=name
+  def init():
+    print 'Hello,World!!',self.name
+s=Person()
+s.superman('Lorry David')
+s.init()
