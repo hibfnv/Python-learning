fff21498b48191f15bc9d57d159f936645d90c26
Eason Tsui <hibfnv@hotmail.com>
2018-09-03 10:05:35 +0800

    Create pass_condition.py

---
+++ pass_condition.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# This is a pass condition in for loop
+# Author : Eason
+for i in 'Python':
+    if i == 'h':
+        pass
+        print "Pass condition"
+    print i
+print "执行完毕，退出。"
