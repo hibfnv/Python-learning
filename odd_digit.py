000897aa4f3801c6e2ea48785978e18b76148d9e
Eason Tsui <hibfnv@hotmail.com>
2018-09-03 09:14:18 +0800

    Create odd_digit.py

---
+++ odd_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# Odd integral number print in screen
+# Author : Eason
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0:
+        continue
+    print n
