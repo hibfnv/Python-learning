4e25d78924732c6a4315be17ba87dd0e17beca15
Eason Tsui <hibfnv@hotmail.com>
2018-09-04 09:33:24 +0800

    Create def_function.py

---
+++ def_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# This is a def function script.
+# Author: Eason
+print "定义一个函数并输出到屏幕上"
+print "=" * 20
+def hell():
+    print "Hello Wolrd!"
+hell()
+print "=" * 20
+print "先定义一个函数，并在函数中定义一个变量，通过函数引用变量并将结果输出到屏幕上。"
+print "=" * 20
+def str(a):
+    a = "Spereme!"
+    print a
+str('a')
+print "=" * 20
+print "执行结束，退出。"
